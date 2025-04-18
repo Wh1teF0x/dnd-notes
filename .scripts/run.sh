1ebf28e51b21c08657ccef029b5abdc270671f1d
Wh1teF0x <untoxited@gmail.com>
2025-04-18 20:41:36 +0300

    fix: script fix

--- .scripts/run.sh
+++ .scripts/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-touch ../__obsidian/index.md
-echo "![[Заметки]]" >>../__obsidian/index.md
+ls
+touch __obsidian/index.md
+echo "![[Заметки]]" >>__obsidian/index.md
 
 npm ci
 
-npx quartz create --strategy copy --links shortest --source ../__obsidian
+npx quartz create --strategy copy --links shortest --source __obsidian
 npx quartz build
